d7d962e135b7c09a298a39b31f7cb93dfab5670d
Josh Harris <49429928+jharris711@users.noreply.github.com>
2020-06-14 14:59:32 -0400

    favicon

--- app/urls.py
+++ app/urls.py
@@ -2,10 +2,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls', namespace='core')),
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url('images/favicon.ico')))
 ]
 
 if settings.DEBUG:
