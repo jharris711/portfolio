b090f394398054fe47ec38cba609b0c4a1d52a7d
Josh Harris <49429928+jharris711@users.noreply.github.com>
2020-06-26 01:40:13 -0400

    contact form style fix

--- core/forms.py
+++ core/forms.py
@@ -5,34 +5,34 @@ class ContactForm(forms.Form):
     name = forms.CharField(
         max_length=50,
         widget=forms.TextInput(attrs={
-            "class": "form-control shadow border-0",
+            "class": "form-control shadow-sm border-0",
             "placeholder": "Name"
         })
     )
     email = forms.EmailField(
         max_length=60,
         widget=forms.TextInput(attrs={
-            "class": "form-control shadow border-0",
+            "class": "form-control shadow-sm border-0",
             "placeholder": "Email"
         })
     )
     phone = forms.CharField(
         max_length=13,
         widget=forms.TextInput(attrs={
-            "class": "form-control shadow border-0",
+            "class": "form-control shadow-sm border-0",
             "placeholder": "Phone"
         })
     )
     subject = forms.CharField(
         max_length=60,
         widget=forms.TextInput(attrs={
-            "class": "form-control shadow border-0",
+            "class": "form-control shadow-sm border-0",
             "placeholder": "Subject"
         })
     )
     message = forms.CharField(
         widget=forms.Textarea(attrs={
-            "class": "form-control shadow border-0",
+            "class": "form-control shadow-sm border-0",
             "placeholder": "Message"
         })
     )
